c3365bd321a2a37b53b260b835687b59e5d448d5
Pieruccicarlos110551 <64793185+Pieruccicarlos110551@users.noreply.github.com>
2021-05-31 23:20:30 -0300

    Continuamos con la tabla ventas

---
+++ CapaDatos/Comprobante.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+  public class Comprobante
+    {
+        private int id_comprobante;
+        private DateTime fecha;
+        private int id_personal;
+        private int id_cliente;
+
+        public int Id_comprobante { get => id_comprobante; set => id_comprobante = value; }
+        public int Id_personal { get => id_personal; set => id_personal = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public int Id_cliente { get => id_cliente; set => id_cliente = value; }
+
+        public Comprobante() { }
+
+        public Comprobante(int id_comprobante, DateTime fecha,int id_personal, int id_cliente) {
+
+            this.Id_comprobante = id_comprobante;
+            this.Fecha = fecha;
+            this.Id_personal = id_personal;
+            this.Id_cliente = id_cliente;
+        }
+
+        public string Insertar_Comprobante(Comprobante comprobante, List<Ddetalle_Comprobante> detalle)
+       {
+            string rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon.ConnectionString = conexion.CadenaConexion;
+                sqlcon.Open();
+                SqlTransaction SqlTra = sqlcon.BeginTransaction();
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.Transaction = SqlTra;
+                sqlcmd.CommandText = "Insertar_Comprobante";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                
+                SqlParameter parid_comprobante = new SqlParameter();
+                parid_comprobante.ParameterName = "@id_comprobante";
+                parid_comprobante.SqlDbType = SqlDbType.Int;
+                parid_comprobante.Direction = ParameterDirection.Output;
+                sqlcmd.Parameters.Add(parid_comprobante);
+
+                SqlParameter parfecha = new SqlParameter();
+                parfecha.ParameterName = "@fecha";
+                parfecha.SqlDbType = SqlDbType.Date;
+                parfecha.Value = comprobante.Fecha;
+                sqlcmd.Parameters.Add(parfecha);
+
+
+                SqlParameter parpersonal = new SqlParameter();
+                parpersonal.ParameterName = "@id_personal";
+                parpersonal.SqlDbType = SqlDbType.Date;
+                parpersonal.Value = comprobante.Id_personal;
+                sqlcmd.Parameters.Add(parpersonal);
+
+                SqlParameter parcliente = new SqlParameter();
+                parcliente.ParameterName = "@id_cliente";
+                parcliente.SqlDbType = SqlDbType.Date;
+                parcliente.Value = comprobante.Id_cliente;
+                sqlcmd.Parameters.Add(parcliente);
+                
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "NO se Ingreso el Registro";
+
+                if (rpta.Equals("OK"))
+                {
+                    this.Id_comprobante = Convert.ToInt32(sqlcmd.Parameters["@id_comprobante"]);
+                    foreach (Ddetalle_Comprobante det in detalle) 
+                    {
+                        det.Id_comprobante = this.Id_comprobante;
+                        rpta = det.Insertar_Detalle_Comprobante(det, ref sqlcon, ref SqlTra);
+                        if (!rpta.Equals("OK"))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (rpta.Equals("OK"))
+                {
+                    SqlTra.Commit();
+                }
+                else { SqlTra.Rollback(); }
+
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+        }
+
+        public string EliminarComprobante(Comprobante comprobante)
+        {
+            string rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon.ConnectionString = conexion.CadenaConexion;
+                sqlcon.Open();
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "Eliminar_Comprobante";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+
+
+                SqlParameter paridcomprobante = new SqlParameter();
+                paridcomprobante.ParameterName = "@id_comprobante";
+                paridcomprobante.SqlDbType = SqlDbType.Int;
+                paridcomprobante.Value = comprobante.Id_comprobante;
+                sqlcmd.Parameters.Add(paridcomprobante);
+
+
+
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "NO se pudo Eliminar el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+
+        }
+
+
+
+
+    }
+}
---
+++ CapaDatos/Ddetalle_Comprobante.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace CapaDatos
+{
+  public class Ddetalle_Comprobante
+    {
+        private int id_detalle_comprobante;
+        private double monto; 
+        private double descuento;
+        private int id_funcion;
+        private int id_butaca;
+        private int id_tipo_compra;
+        private int cantidad;
+        private int id_pelicula;
+        private int id_forma_pago;
+        private int id_sala;
+        private int id_tipo_sala;
+        private int id_comprobante;
+        private int res, n1, n2, n3;
+
+      /*  public void Sumar(TextBox uno, TextBox dos, TextBox tres, TextBox respu)
+        {
+            n1 = int.Parse(uno.Text);
+            n2 = int.Parse(dos.Text);
+            n3 = int.Parse(tres.Text);
+            res = n1 + n2 + n3;
+            respu.Text = res.ToString();
+        }*/
+
+        public Ddetalle_Comprobante(){}
+
+
+        public int Id_detalle_comprobante { set { id_detalle_comprobante = value; } get { return id_detalle_comprobante; } }
+        public double Monto { set { monto = value; } get { return monto; } }
+        public double Descuento { set { descuento = value; } get { return descuento; } }
+        public int Id_funcion { set { id_funcion = value; } get { return id_funcion; } }
+        public int Id_butaca { set { id_butaca = value; } get { return id_butaca; } }
+        public int Id_tipo_compra { set { id_tipo_compra = value; } get { return id_tipo_compra; } }
+        public int Cantidad { set { cantidad = value; } get { return cantidad; } }
+        public int Id_pelicula { set { id_pelicula = value; } get { return id_pelicula; } }
+        public int Id_forma_pago { set { id_forma_pago = value; } get { return id_forma_pago; } }
+        public int Id_sala { set { id_sala = value; } get { return id_sala; } }
+        public int Id_tipo_sala { set { id_tipo_sala = value; } get { return id_tipo_sala; } }
+        public int Id_comprobante { set { id_comprobante = value; } get { return id_comprobante; } }
+       
+
+
+
+        public Ddetalle_Comprobante(int id_detalle_comprobante, double monto, double descuento, 
+            int id_funcion, int id_butaca, int id_tipo_compra, int cantidad, int id_pelicula,
+            int id_forma_pago, int id_sala, int id_tipo_sala, int id_comprobante) {
+
+            this.Id_detalle_comprobante = id_detalle_comprobante;
+            this.Monto = monto;
+            this.Descuento = descuento;
+            this.Id_funcion = id_funcion;
+            this.Id_butaca = id_butaca;
+            this.Id_tipo_compra = id_tipo_compra;
+            this.Cantidad = cantidad;
+            this.Id_pelicula = id_pelicula;
+            this.Id_forma_pago = id_forma_pago;
+            this.Id_sala = id_sala;
+            this.Id_tipo_sala = id_tipo_sala;
+            this.Id_comprobante = id_comprobante;
+        }
+
+        public string Insertar_Detalle_Comprobante(Ddetalle_Comprobante dcomprobante,
+        ref SqlConnection sqlcon, ref SqlTransaction sqlTra)
+        {
+
+            string rpta = "";
+           
+            try
+            {
+                sqlcon.ConnectionString = conexion.CadenaConexion;
+                sqlcon.Open();
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "Insertar_Detalle_Comprobante";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter parid_detalle_comprobante = new SqlParameter();
+                parid_detalle_comprobante.ParameterName = "@id_detalle_comprobante";
+                parid_detalle_comprobante.SqlDbType = SqlDbType.Int;
+                parid_detalle_comprobante.Direction = ParameterDirection.Output;
+                sqlcmd.Parameters.Add(parid_detalle_comprobante);
+                
+                SqlParameter parmonto = new SqlParameter();
+                parmonto.ParameterName = "@monto";
+                parmonto.SqlDbType = SqlDbType.Float;
+                parmonto.Value = dcomprobante.Monto;
+                sqlcmd.Parameters.Add(parmonto);
+
+                SqlParameter pardescuento = new SqlParameter();
+                pardescuento.ParameterName = "@descuento";
+                pardescuento.SqlDbType = SqlDbType.Float;
+                pardescuento.Value = dcomprobante.Descuento;
+                sqlcmd.Parameters.Add(pardescuento);
+
+                SqlParameter parfuncion = new SqlParameter();
+                parfuncion.ParameterName = "@id_funcion";
+                parfuncion.SqlDbType = SqlDbType.Int;
+                parfuncion.Value = dcomprobante.Id_funcion;
+                sqlcmd.Parameters.Add(parfuncion);
+
+                SqlParameter parbutaca = new SqlParameter();
+                parbutaca.ParameterName = "@id_butaca";
+                parbutaca.SqlDbType = SqlDbType.Int;
+                parbutaca.Value = dcomprobante.Id_butaca;
+                sqlcmd.Parameters.Add(parbutaca);
+
+                SqlParameter partipocompra = new SqlParameter();
+                partipocompra.ParameterName = "@id_tipo_compra";
+                partipocompra.SqlDbType = SqlDbType.Int;
+                partipocompra.Value = dcomprobante.Id_tipo_compra;
+                sqlcmd.Parameters.Add(partipocompra);
+
+                SqlParameter parcantidad = new SqlParameter();
+                parcantidad.ParameterName = "@cantidad";
+                parcantidad.SqlDbType = SqlDbType.Int;
+                parcantidad.Value = dcomprobante.Cantidad;
+                sqlcmd.Parameters.Add(parcantidad); 
+
+                SqlParameter parpelicula = new SqlParameter();
+                parpelicula.ParameterName = "@id_pelicula";
+                parpelicula.SqlDbType = SqlDbType.Int;
+                parpelicula.Value = dcomprobante.Id_pelicula;
+                sqlcmd.Parameters.Add(parpelicula); 
+
+                SqlParameter parformapago = new SqlParameter();
+                parformapago.ParameterName = "@id_forma_pago";
+                parformapago.SqlDbType = SqlDbType.Int;
+                parformapago.Value = dcomprobante.Id_forma_pago;
+                sqlcmd.Parameters.Add(parformapago);
+
+                SqlParameter parsala = new SqlParameter();
+                parsala.ParameterName = "@id_sala";
+                parsala.SqlDbType = SqlDbType.Int;
+                parsala.Value = dcomprobante.Id_sala;
+                sqlcmd.Parameters.Add(parsala);
+
+                SqlParameter partiposala = new SqlParameter();
+                partiposala.ParameterName = "@id_tipo_sala";
+                partiposala.SqlDbType = SqlDbType.Int;
+                partiposala.Value = dcomprobante.Id_tipo_sala;
+                sqlcmd.Parameters.Add(partiposala);
+
+                SqlParameter parid_comprobante = new SqlParameter();
+                parid_comprobante.ParameterName = "@id_comprobante";
+                parid_comprobante.SqlDbType = SqlDbType.Int;
+                parid_comprobante.Value = dcomprobante.Id_comprobante;
+                sqlcmd.Parameters.Add(parid_comprobante);
+                
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "NO se Ingreso el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+        }
+
+
+
+
+    }
+}
